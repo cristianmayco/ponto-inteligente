da29d5753aca1f974e3e59f408b5b9b65c6d4be2
Cristian Mayco da Costa <cristianmayco@gmail.com>
2020-09-25 17:42:10 -0300

    Added repositories

---
+++ src/main/java/com/pontointeligente/pontointeligente/api/repository/EmpresaRepository.java
@@ -0,0 +1,11 @@
+package com.pontointeligente.pontointeligente.api.repository;
+
+import com.pontointeligente.pontointeligente.api.model.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    @Transactional(readOnly = true)
+    Empresa findByCnpj(String cnpj);
+}
