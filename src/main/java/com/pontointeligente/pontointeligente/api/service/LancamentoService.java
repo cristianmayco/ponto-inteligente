7c47f6296b8c72840c811397c3574df6c70f7416
Cristian Mayco da Costa <cristianmayco@gmail.com>
2020-09-26 14:19:18 -0300

    Removed unnecessary import

--- src/main/java/com/pontointeligente/pontointeligente/api/service/LancamentoService.java
+++ src/main/java/com/pontointeligente/pontointeligente/api/service/LancamentoService.java
@@ -2,7 +2,6 @@ package com.pontointeligente.pontointeligente.api.service;
 
 import com.pontointeligente.pontointeligente.api.model.Lancamento;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
